1cb7564ca252446b6b938e956a4c1bfafbdf2b33
Iryna <77626508+irynapytailo@users.noreply.github.com>
2024-09-05 14:36:28 +0300

    Create task2Extra.asm

---
+++ task2/task2Extra.asm
@@ -0,0 +1,37 @@
+
+.386
+.model flat,stdcall
+.stack 4096
+ExitProcess proto,dwExitCode:dword
+
+.data
+source byte "i love assembler",0
+nameSize = ($ - aName) - 1
+
+.code
+main proc
+
+; Push the name on the stack.
+
+mov	 ecx,nameSize
+	mov	 esi,0
+
+L1:	movzx eax,source[esi]	
+	push eax				; push character on stack
+	inc	 esi
+	loop L1
+
+
+
+mov	 ecx,nameSize
+	mov	 esi,0
+
+L2:	pop  eax				; get character
+	and al, 0DFh			;make uppercase
+	mov	 aName[esi],al		; store in string
+	inc	 esi
+	loop L2
+
+	Invoke ExitProcess,0
+main endp
+end main
